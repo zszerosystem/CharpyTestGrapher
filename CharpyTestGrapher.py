c0263b537b8e0925a2053e24cecf9c4a749b55b3
Tim Moulding <90204228+zszerosystem@users.noreply.github.com>
2022-03-03 22:00:04 +0000

    Update CharpyTestGrapher.py

--- CharpyTestGrapher.py
+++ CharpyTestGrapher.py
@@ -7,52 +7,36 @@ from scipy.interpolate import make_interp_spline
 from scipy.interpolate import interp1d
 import scipy.interpolate as spi
 
-"this is for percentage crystallinity vs temp"
-# # graph=np.array([200,0],[0,10],[-20,90],[-80,100],[-195,100])
-# # cline=np.array([200,50],[0,50],[-20,50],[-80,50],[-195,50])
-# gx=np.array([200,0,-20,-80,-195])
-# gy=np.array([0,0,0,0,0])
-
-# lx=np.array([200,0,-20,-80,-195])
-# ly=np.array([50,50,50,50,50])
-
-# auxliney=np.arange(0,51)
-# auxlinex=np.array([-45]*51)
-
-# plt.plot(gx,gy)
-# # plt.plot(lx,ly,':')
-# # plt.plot(auxlinex,auxliney,':')
-# plt.xlabel("Temperature℃")
-# plt.ylabel("Percentage Crystallinity")
-
-# x=np.linspace(-20,0,1000)
-# m=(10-90)/(0-(-20))
-
-"this is for impact energy "
-
 "temperature"
-x = np.array([-195, -80, -20, 0, 20,50,80, 100, 200]) 
-'energy'
-y = np.array([2, 3, 12, 24, 158, 149,260,271,258])  
+x = np.array([-195,-80,-20,0,200]) 
+"energy/percentage crystallinity"
+y = np.array([100,100,90,10,0])  
 
 "interpolation"
+#using akima 1d interpolation
 X_Y_Spline = spi.Akima1DInterpolator(x, y)
+#using cubic spline interpolation
+X_Y_Spline = make_interp_spline(x, y,k=3)
+cubic_interploation_model = interp1d(x, y, kind = "cubic")
 
-# X_Y_Spline = make_interp_spline(x, y,k=3)
-# cubic_interploation_model = interp1d(x, y, kind = "cubic")
 X_ = np.linspace(x.min(), x.max(), 1000)
 Y_ = X_Y_Spline(X_)
 plt.plot(X_, Y_)
-# plt.plot(x,y)
+
+"you can plot without interpolation"
+plt.plot(x,y)
 
 "finding intersection"
 avg=(np.min(y)+np.max(y))/2
 lx=np.linspace(-195,200,1000)
-ly=np.array([avg]*1000)
+#"this is for fracture energy"
+ly=np.array([avg]*1000) 
+#"this is for percentage crystallinity"
+ly=np.array([50]*1000) 
 
 idx = np.argwhere(np.diff(np.sign(Y_-ly))).flatten()
 plt.plot(X_[idx], ly[idx],'ro')
-print(lx[idx])
+print(lx[idx]) #finding x point of intersection
 
 
 "construction lines"
@@ -62,9 +46,10 @@ auxliney=np.arange(0,200)
 auxlinex=np.array([lx[idx]]*200)
 plt.plot(auxlinex,auxliney,":")
 plt.xlabel("Temperature(℃)")
-plt.ylabel("Energy(J)")
+plt.ylabel("Percentage crystallinty")
 
-
-
+"controlling yaxis limits"
+ax=plt.gca()
+ax.set_ylim([0, 200])
 
 plt.show()
